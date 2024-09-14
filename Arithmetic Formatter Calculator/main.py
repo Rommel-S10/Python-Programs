da7d95a775ff14d190d6f1ad898f22e8067eabeb
Rommel Soriano <rommelariel10@gmail.com>
2024-09-13 20:08:13 -0400

    AFC four errors checked. One more to go

--- Arithmetic Formatter Calculator/main.py
+++ Arithmetic Formatter Calculator/main.py
@@ -1,5 +1,20 @@
 def arithmetic_arranger(problems, show_answers=False):
+    alphabet = 'abcdefghijklmnopkrstuvwxyz'
+
+    if len(problems) > 5: #checks that the list of objects is less than 5
+        return 'Error: Too many problems.'
+
+    for list_of_numbers in problems:  #this will check that every object has a + or - operation sign
+        if "+" not in list_of_numbers and '-' not in list_of_numbers:
+            return "Error: Operator must be '+' or '-'."
+
+    for list_of_numbers in problems:  #this will go through every operator problem
+        for numbers in list_of_numbers: # this will go through every number to make sure that is a digit.
+            result = alphabet.find(numbers)
+            if result != -1: # if it is -1 then it means that a letter was not found
+                return 'Error: Numbers must only contain digits. '
 
     return problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+
+print(f'\n{arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"])}')
